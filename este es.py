bb90161832e94afcc784ce710e9841f2700cb8ae
Guatoncito <benja_as@hotmail.com>
2014-08-25 17:51:20 -0400

    asd

--- este es.py
+++ este es.py
@@ -3,6 +3,6 @@ import random
 
 Random=palabras('texto_a_leer.txt')
 Texto=open('texto_generado.txt','w')
-palabra_inicial=Random[random.random()*10]
+palabra_inicial=Random[randrange(len(Random))]
 write(palabra_inicial)
 
