9a714a70cbba291bc46807fb844a6af80dfbb522
Guatoncito <benja_as@hotmail.com>
2014-08-25 19:47:25 -0400

    jeje

--- este es.py
+++ este es.py
@@ -5,6 +5,13 @@ Random=palabras('texto_a_leer.txt')
 Texto=open('texto_generado.txt','w')
 palabra_inicial=Random[randrange(len(Random))]
 write(palabra_inicial)
-
+h=histograma('texto_a_leer.txt')
+tran=transicion('texto_a_leer.txt',h)
+palabra_actual=palabra_inicial
 while True:
-
+	a=palabra_despues_otra_posibilidad('texto_a_leer.txt',palabra_actual)
+	b=dict()
+	tot=0
+	for i in a.values():
+		tot+=i
+	
