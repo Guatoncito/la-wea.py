22cf76533df8b2039d25b787d681dc9c5ef6a39a
Guatoncito <benja_as@hotmail.com>
2014-08-25 20:47:15 -0400

    casi listo ctm

--- este es.py
+++ este es.py
@@ -3,20 +3,19 @@ from random import *
 
 Random=palabras('texto_a_leer.txt')
 Texto=open('texto_generado.txt','w')
-palabra_inicial=Random[randrange(len(Random))]
-write(palabra_inicial)
-h=histograma('texto_a_leer.txt')
-tran=transicion('texto_a_leer.txt',h)
+palabra_inicial='Los'
+Texto.write(palabra_inicial+' ')
 palabra_actual=palabra_inicial
 cont=0
 while True:
-	a=palabra_despues_otra_posibilidad('texto_a_leer.txt',palabra_actual)
+	a=palabra_despues_otra_posibilidades('texto_a_leer.txt',palabra_actual)
 	b=list()
 	for i in a:
 		for x in range(a[i]):
 			b.append(i)
 	palabra_actual=choice(b)
 	Texto.write(palabra_actual)
+	Texto.write(' ')
 	cont+=1
 	if cont==50:
 		break
